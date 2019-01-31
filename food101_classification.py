465184833fd85648bba2dcd807de0f51d429227c
saikat-roy <saikatroy@uni-bonn>
2019-01-31 04:56:24 +0100

    food 101 module updated

--- food101_classification.py
+++ food101_classification.py
@@ -86,7 +86,7 @@ if __name__ == "__main__":
         X = X.to("cuda:0")
         i+=X.size(0)
         train_x[(epoch_id)*X.size(0):min((epoch_id+1)*X.size(0),train_x.shape[0]),:] = disc_new(X).detach().cpu().numpy()
-        train_y[(epoch_id) * X.size(0):min((epoch_id+1) * X.size(0),print(train_y.shape[0]))] = y.numpy()
+        train_y[(epoch_id)*X.size(0):min((epoch_id+1)*X.size(0),train_y.shape[0])] = y.numpy()
         #print(train_y[(epoch_id) * X.size(0):(epoch_id+1) * X.size(0)])
     print(i)
     for epoch_id, (X, y) in enumerate(validation_loader):
